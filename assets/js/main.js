0243454ee66e5ae00cef3003b536e58fe378a4eb
DeionSi <DeionSi@users.noreply.github.com>
2023-01-12 17:27:12 +0100

    Add breakpoints

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,13 +11,13 @@
 		$sidebar = $('#sidebar');
 
 	// Breakpoints. // Commented as the function is not implemented
-		/*breakpoints({
+		breakpoints({
 			xlarge:   [ '1281px',  '1680px' ],
 			large:    [ '981px',   '1280px' ],
 			medium:   [ '737px',   '980px'  ],
 			small:    [ '481px',   '736px'  ],
 			xsmall:   [ null,      '480px'  ]
-		});*/
+		});
 
 	// Play initial animations on page load.
 		$window.on('load', function() {
