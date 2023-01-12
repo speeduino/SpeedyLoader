e650a08eb15d51a18efc9bc479909f3da8034822
DeionSi <DeionSi@users.noreply.github.com>
2023-01-12 17:09:23 +0100

    Function is not available

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,14 +10,14 @@
 		$body = $('body'),
 		$sidebar = $('#sidebar');
 
-	// Breakpoints.
-		breakpoints({
+	// Breakpoints. // Commented as the function is not implemented
+		/*breakpoints({
 			xlarge:   [ '1281px',  '1680px' ],
 			large:    [ '981px',   '1280px' ],
 			medium:   [ '737px',   '980px'  ],
 			small:    [ '481px',   '736px'  ],
 			xsmall:   [ null,      '480px'  ]
-		});
+		});*/
 
 	// Hack: Enable IE flexbox workarounds.
 		if (browser.name == 'ie')
